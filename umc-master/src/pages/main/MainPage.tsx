e4294ebfa2dfad709e0c1681f8e5a4186163ad28
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-01-07 23:39:43 +0900

    ✨ feat : TipsSection 컴포넌트 구현 #11

--- umc-master/src/pages/main/MainPage.tsx
+++ umc-master/src/pages/main/MainPage.tsx
@@ -2,6 +2,16 @@ import Banner from './components/Banner';
 import InterestsAndCategories from './components/Categories';
 import QuizBox from './components/QuizBox';
 import SearchSection from './components/SearchSection';
+import TipsSection from './components/TipsSection';
+import TipsSectionTopFive from './components/TipsSectionTopFive';
+
+const dummyData = [
+  { image: '#', text: '오늘의 추천입니다.' },
+  { image: '#', text: '여행을 떠나요.' },
+  { image: '#', text: '오늘의 추천입니다.' },
+  { image: '#', text: '오늘의 추천입니다.' },
+  { image: '#', text: '오늘의 추천입니다.' },
+];
 
 const MainPage: React.FC = () => {
   return (
@@ -11,6 +21,9 @@ const MainPage: React.FC = () => {
         <QuizBox></QuizBox>
         <SearchSection></SearchSection>
         <InterestsAndCategories></InterestsAndCategories>
+        <TipsSectionTopFive title="이 주의 꿀팁 BEST 5" items={dummyData} />
+        <TipsSection title="실시간 꿀팁" items={dummyData} />
+        <TipsSection title="오늘의 꿀팁" items={dummyData} />
       </div>
     </>
   );
