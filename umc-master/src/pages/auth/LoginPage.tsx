85315e6a5c0b06bba18b8781652ba2c08274168a
rael <ekosemily@gmail.com>
2025-01-08 03:09:16 +0900

    ✨ feat: LoginPage LoginOptions UI 구현 #10

--- umc-master/src/pages/auth/LoginPage.tsx
+++ umc-master/src/pages/auth/LoginPage.tsx
@@ -12,17 +12,19 @@ const LoginPage: React.FC = () => {
           </LoginTitle>
           <LoginInputForm>
             <LoginInput>
-              <Input 
-                type={'email'} 
-                placeholder={'이메일 입력하기'} 
-              />
-              <Input
-                type={'password'} 
-                placeholder={'비밀번호 입력하기'} 
-              />
+              <Input type={'email'} placeholder={'이메일 입력하기'} />
+              <Input type={'password'} placeholder={'비밀번호 입력하기'} />
             </LoginInput>
             <LoginDetail>
-
+              <AutoLoginWrapper>
+                <AutoLoginCheckbox type={'checkbox'} />
+                <AutoLoginLabel>자동로그인</AutoLoginLabel>
+              </AutoLoginWrapper>
+              <Options>
+                <TextButton>회원 정보 찾기</TextButton>
+                <Separator />
+                <TextButton>회원 가입</TextButton>
+              </Options>
             </LoginDetail>
           </LoginInputForm>
         </LoginForm>
@@ -127,5 +129,57 @@ const Input = styled.input`
 `
 
 const LoginDetail = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`
 
+const AutoLoginWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+
+const AutoLoginCheckbox = styled.input`
+  width: 23px;
+  height: 23px;
+  accent-color: #CCCCCC;
+`
+
+const AutoLoginLabel = styled.div`
+  color: var(--Text-gray, #636363);
+
+  /* Body/small */
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  letter-spacing: -0.36px;
+`
+
+const Options = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`
+
+const TextButton = styled.div`
+  color: var(--Text-gray, #636363);
+
+  /* Body/small */
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  letter-spacing: -0.36px;
+  cursor: pointer;
+`;
+
+const Separator = styled.div`
+  width: 1px;
+  height: 30px;
+  background-color: var(--Color-gray, #9C9C9C);
 `
