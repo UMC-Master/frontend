538ef0bb761bab98f02347759354ea71a5fc7d7e
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-01-07 18:33:16 +0900

    ✨ feat: QuizBox 컴포넌트 구현 #11

--- umc-master/src/pages/main/MainPage.tsx
+++ umc-master/src/pages/main/MainPage.tsx
@@ -1,10 +1,12 @@
 import Banner from './components/Banner';
+import QuizBox from './components/QuizBox';
 
 const MainPage: React.FC = () => {
   return (
     <>
       <div>
         <Banner></Banner>
+        <QuizBox></QuizBox>
       </div>
     </>
   );
