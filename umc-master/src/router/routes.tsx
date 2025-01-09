1d023a6e1a34d4c75974f3bf9475c607360ea2bd
Minji Kim <minjikim2000@hotmail.com>
2025-01-10 01:52:27 +0900

    💄 UI: 헤더 푸터 레이아웃 추가 #12

--- umc-master/src/router/routes.tsx
+++ umc-master/src/router/routes.tsx
@@ -11,22 +11,25 @@ import CreatePostPage from '@pages/createPost/CreatePostPage';
 import CommunityPage from '@pages/community/CommunityPage';
 import MagazinePage from '@pages/magazine/MagazinePage';
 import MagazineDetailPage from '@pages/magazine/MagazineDetailPage';
+import RootLayout from '@layouts/root-layout';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={RoutePaths.LANDING} element={<LandingPage />} />
-        <Route path={RoutePaths.LOGIN} element={<LoginPage />} />
-        <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
-        <Route path={RoutePaths.MAIN} element={<MainPage />} />
-        <Route path={RoutePaths.SEARCH} element={<SearchPage />} />
-        <Route path={RoutePaths.SAVE_TIP} element={<SaveTipPage />} />
-        <Route path={RoutePaths.SAVE_TIP_DETAIL} element={<SaveTipDetailPage />} />
-        <Route path={RoutePaths.CREATE_POST} element={<CreatePostPage />} />
-        <Route path={RoutePaths.COMMUNITY} element={<CommunityPage />} />
-        <Route path={RoutePaths.MAGAZINE} element={<MagazinePage />} />
-        <Route path={RoutePaths.MAGAZINE_DETAIL} element={<MagazineDetailPage />} />
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path={RoutePaths.LOGIN} element={<LoginPage />} />
+          <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
+          <Route path={RoutePaths.MAIN} element={<MainPage />} />
+          <Route path={RoutePaths.SEARCH} element={<SearchPage />} />
+          <Route path={RoutePaths.SAVE_TIP} element={<SaveTipPage />} />
+          <Route path={RoutePaths.SAVE_TIP_DETAIL} element={<SaveTipDetailPage />} />
+          <Route path={RoutePaths.CREATE_POST} element={<CreatePostPage />} />
+          <Route path={RoutePaths.COMMUNITY} element={<CommunityPage />} />
+          <Route path={RoutePaths.MAGAZINE} element={<MagazinePage />} />
+          <Route path={RoutePaths.MAGAZINE_DETAIL} element={<MagazineDetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
