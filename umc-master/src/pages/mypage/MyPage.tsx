6e1b8f6853ef9f4cf096ee20d9580fa0ff090257
rael <ekosemily@gmail.com>
2025-01-11 03:49:51 +0900

    ✨ feat: MyPage Best 꿀팁 선정 횟수 & 나의 관심사 컴포넌트 #16

--- umc-master/src/pages/mypage/MyPage.tsx
+++ umc-master/src/pages/mypage/MyPage.tsx
@@ -20,6 +20,7 @@ const dummyData = [
   },
 ];
 
+const dummyInterests = ['청소', '요리', '재활용'];
 
 interface TipcardProps {
   items : { image: string; text: string; id: string }[];
@@ -56,6 +57,35 @@ const MyPage: React.FC<TipcardProps> = ({ items = dummyData }) => {
                   ))}
                 </TipCardList>
               </RecentGoodTip>
+              <Best_Interest>
+                <BestGoodTip>
+                  <Best_InterestTitle>
+                    <TipTitle>Best 꿀팁 선정 횟수</TipTitle>
+                    <GoTo>
+                      <GoToText>보러가기</GoToText>
+                      <GoToText>{'>'}</GoToText>
+                    </GoTo>
+                  </Best_InterestTitle>
+                  <BestChoice>
+                    <BestNum>10</BestNum>
+                    <Bestber>회</Bestber>
+                  </BestChoice>
+                </BestGoodTip>
+                <Interest>
+                  <Best_InterestTitle>
+                    <TipTitle>나의 관심사</TipTitle>
+                    <GoTo>
+                      <GoToText>편집하기</GoToText>
+                      <GoToText>{'>'}</GoToText>
+                    </GoTo>
+                  </Best_InterestTitle>
+                  <InterestTagList>
+                    {dummyInterests.map((category, index) => (
+                      <Tag key={index}>#{category}</Tag>
+                    ))}
+                  </InterestTagList>
+                </Interest>
+              </Best_Interest>
             </ProfileCard2>
           </MyPageForm>
         </Container>
@@ -70,7 +100,6 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 90px 0;
   background: #FFF;
 `
 
@@ -78,7 +107,7 @@ const MyPageForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 68px 241px 101px;
+  padding: 68px 241px 101px 242px;
 `
 
 const Title = styled.h6`
@@ -210,6 +239,7 @@ const ProfileEdit = styled.button`
   font-weight: 400;
   line-height: 36px;
   letter-spacing: -0.48px;
+  cursor: pointer;
 
   &:hover {
     background: var(--Main-600, #157a66);
@@ -217,8 +247,11 @@ const ProfileEdit = styled.button`
 `
 
 const ProfileCard2 = styled.div`
+  display: flex;
+  flex-direction: row;
   width: 1415px;
   height: 372px;
+  gap: 162px;
   flex-shrink: 0;
   padding: 57px 9px 0px 13px; /* top right bottom left */
 `
@@ -231,7 +264,7 @@ const RecentGoodTip = styled.div`
   align-items: flex-start;
   gap: 50px;
   flex-shrink: 0;
-  padding: 45px 162px 32px 0px; /* top right bottom left */
+  padding: 45px 0px 32px 0px; /* top right bottom left */
 `
 
 const TipTitle = styled.div`
@@ -257,3 +290,100 @@ const TipCardList = styled.div`
   gap: 36px;
 `
 
+const Best_Interest = styled.div`
+  display: flex;
+  width: 630px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 47px;
+  padding: 0px 9px 0px 0px; /* top right bottom left */
+`
+
+const BestGoodTip = styled.div`
+  display: flex;
+  width: 630px;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 50px;
+`
+
+const Best_InterestTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`
+
+const GoTo = styled.div`
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  gap: 10px;
+  cursor: pointer;
+`
+
+const GoToText = styled.div`
+  color: var(--Text-gray, #636363);
+  font-family: Pretendard;
+  font-size: 24px;
+  font-weight: 400;
+  letter-spacing: -0.48px;
+`
+
+const Interest = styled.div`
+  display: flex;
+  width: 630px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 50px;
+`
+
+const BestChoice = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const BestNum = styled.div`
+  color: var(--Main-800, #084951);
+
+  /* Display/medium */
+  font-family: Pretendard;
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 75px; /* 133.929% */
+  letter-spacing: 0.56px;
+`
+
+const Bestber = styled.div`
+  color: var(--Main-800, #084951);
+
+  /* Heading/large */
+  font-family: Pretendard;
+  font-size: 50px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 75px; /* 150% */
+  letter-spacing: 0.5px;
+`
+
+const InterestTagList = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`
+
+const Tag = styled.span<{ selected?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 17px 34px;
+  border-radius: 30px;
+  font-size: 24px;
+  min-width: 103px;
+  height: 60px;
+  color: #ffffff;
+  background-color: #1B8C78;
+  cursor: pointer;
+`
