d67e68fc93eae5ea38d1b3940e3fb400f422b163
rael <ekosemily@gmail.com>
2025-01-13 01:07:28 +0900

    ♻️ refactor: MyPage Section.tsx 컴포넌트 분리 #16

--- umc-master/src/pages/mypage/MyPage_components/BestInterest.tsx
+++ umc-master/src/pages/mypage/MyPage_components/BestInterest.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import styled from 'styled-components';
 import InterestTag from './InterestTag';
+import Section from './Section';
 
 interface BestInterestProps {
     interests: string[];
@@ -11,31 +12,33 @@ const BestInterest: React.FC<BestInterestProps> = ({ interests }) => {
     <>
       <BestInterestContainer>
         <Section_1>
-          <SectionHeader>
-            <TipTitle>Best 꿀팁 선정 횟수</TipTitle>
-            <GoTo>
-              <GoToText>보러가기</GoToText>
-              <GoToText>{'>'}</GoToText>
-            </GoTo>
-          </SectionHeader>
-          <BestChoice>
-            <BestNum>10</BestNum>
-            <BestNumber>회</BestNumber>
-          </BestChoice>
+          <Section 
+            title='Best 꿀팁 선정 횟수'
+            content={
+              <>
+                <BestChoice>
+                  <BestNum>10</BestNum>
+                  <BestNumber>회</BestNumber>
+                </BestChoice>
+              </>
+            }
+            goToText='보러가기'
+          />
         </Section_1>
         <Section_2>
-          <SectionHeader>
-            <TipTitle>나의 관심사</TipTitle>
-            <GoTo>
-              <GoToText>편집하기</GoToText>
-              <GoToText>{'>'}</GoToText>
-            </GoTo>
-          </SectionHeader>
-          <InterestTagList>
-            {interests.map((interests, index) => (
-                <InterestTag key={index} label={interests}></InterestTag>
-            ))}
-          </InterestTagList>
+          <Section 
+            title='나의 관심사'
+            content={
+              <>
+                <InterestTagList>
+                  {interests.map((interest, index) => (
+                      <InterestTag key={index} label={interest}></InterestTag>
+                  ))}
+                </InterestTagList>
+              </>
+            }
+            goToText='편집하기'
+          />
         </Section_2>
       </BestInterestContainer>
     </>
@@ -43,20 +46,6 @@ const BestInterest: React.FC<BestInterestProps> = ({ interests }) => {
 };
 
 export default BestInterest;
-
-const TipTitle = styled.div`
-  width: 623px;
-  align-self: stretch;
-  color: var(--Main-600, #137870);
-
-  /* Heading/xsmall */
-  font-family: Pretendard;
-  font-size: 30px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 45px; /* 150% */
-  letter-spacing: 0.3px;
-`
 
 const BestInterestContainer = styled.div`
   display: flex;
@@ -75,29 +64,6 @@ const Section_1 = styled.div`
   flex-direction: column;
   align-items: flex-end;
   gap: 50px;
-`
-
-const SectionHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  align-self: stretch;
-`
-
-const GoTo = styled.div`
-  display: flex;
-  align-items: center;
-  flex-shrink: 0;
-  gap: 10px;
-  cursor: pointer;
-`
-
-const GoToText = styled.div`
-  color: var(--Text-gray, #636363);
-  font-family: Pretendard;
-  font-size: 24px;
-  font-weight: 400;
-  letter-spacing: -0.48px;
 `
 
 const Section_2 = styled.div`
