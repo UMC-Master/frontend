9d9c7920a02ee2fda94597ed036a1afa3d415c32
Rael <emilyekos@gmail.com>
2025-01-20 20:31:28 +0900

    ✨ feat: 입력창 추가 #21

--- umc-master/src/pages/auth/Signup_components/InputForm.tsx
+++ umc-master/src/pages/auth/Signup_components/InputForm.tsx
@@ -1,13 +1,32 @@
-
+import styled from "styled-components";
+import Input from "@components/Input/Input";
 
 const InputForm: React.FC = () => {
 
   
   return (
     <>
-
+        <SignupInputForm>
+          <Input/>
+          <Input/>
+        </SignupInputForm>
+        <SignupInputForm>
+          <Input/>
+          <Input/>
+        </SignupInputForm>
+        <SignupInputForm>
+          <Input/>
+        </SignupInputForm>
     </>
   );
 };
 
 export default InputForm;
+
+const SignupInputForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  align-self: stretch;
+`
