d711db0d27eac471d2aeeeece9d38c82b28c7428
Rael <emilyekos@gmail.com>
2025-01-20 17:28:29 +0900

    ✨ feat: 프로필 아바타 생성  #21

--- umc-master/src/pages/auth/Signup_components/Profile.tsx
+++ umc-master/src/pages/auth/Signup_components/Profile.tsx
@@ -1,13 +1,38 @@
-
+import { useRef, useState } from "react";
+import styled from "styled-components";
 
 const Profile: React.FC = () => {
 
+  const [Image, setImage] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
+  const fileInput = useRef<HTMLInputElement>(null)
+
+  return (
+    <ProfileImg>
+      <Avatar 
+        src={Image} 
+        alt="Profile Avatar"
+        onClick={()=>{fileInput.current?.click()}}/>
+    </ProfileImg>
+  );
+};
   
-    return (
-      <>
-  
-      </>
-    );
-  };
-  
-  export default Profile;
+export default Profile;
+
+const ProfileImg = styled.div`
+  display: flex;
+  width: 180px;
+  height: 180px;
+  padding: 60px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+`
+
+const Avatar = styled.img`
+  width: 180px;
+  height: 180px;
+  position: absolute;
+  fill: #D9D9D9;
+  border-radius: 50%;
+`
