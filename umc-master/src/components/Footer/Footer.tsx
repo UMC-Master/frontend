e5985df2fc810e3f902bd07bd05ae8ae09fa7edf
Minji Kim <minjikim2000@hotmail.com>
2025-01-20 16:56:49 +0900

    💄 UI: 푸터 수정사항 반영 #25

--- umc-master/src/components/Footer/Footer.tsx
+++ umc-master/src/components/Footer/Footer.tsx
@@ -1,58 +1,55 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import LogoImage from '@assets/logo.png';
+import LogoImage from '@assets/logo-icon.png';
+import Typography from '@components/common/typography';
 
 const Footer: React.FC = () => (
   <FooterContainer>
     {/* TODO: 라우팅 수정 예정 */}
-    <FooterLinks>
-      <Link to="/privacy-policy">개인정보처리방침</Link>
-      <Divider>|</Divider>
-      <Link to="/terms">책임의 한계와 법적 고지</Link>
-      <Divider>|</Divider>
-      <Link to="/support">회원정보 고객센터</Link>
-    </FooterLinks>
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 8 }}>
-      <Logo src={LogoImage} alt="Logo" />
-      <Copyright>Copyright © H_MASTER Corp. All Rights Reserved.</Copyright>
-    </div>
+    <Typography variant="bodySmall">
+      <FooterLinks>
+        <Link to="/privacy-policy">개인정보처리방침</Link>
+        <Divider>|</Divider>
+        <Link to="/terms">책임의 한계와 법적 고지</Link>
+        <Divider>|</Divider>
+        <Link to="/support">회원정보 고객센터</Link>
+      </FooterLinks>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 20 }}>
+        <Logo src={LogoImage} alt="Logo" />
+        <Copyright>Copyright © HOME MASTER Corp. All Rights Reserved.</Copyright>
+      </div>
+    </Typography>
   </FooterContainer>
 );
 
 export default Footer;
 
 const FooterContainer = styled.footer`
-  background: #f8f8f8;
-  padding: 28px;
+  background: #f1f1f1;
+  padding: 40px;
   text-align: center;
-  font-size: 14px;
-  color: #555;
 `;
 
 const FooterLinks = styled.div`
   margin-bottom: 16px;
+  font-size: 14px;
 
   a {
-    color: #555;
+    color: ${({ theme }) => theme.colors.text.black};
     margin: 0 8px;
-
-    &:hover {
-      color: #000;
-    }
   }
 `;
 
 const Divider = styled.span`
-  margin: 0 8px;
-  color: #ccc;
+  margin: 0 14px;
+  color: ${({ theme }) => theme.colors.text.gray};
 `;
 
 const Logo = styled.img`
   height: 30px;
-  cursor: pointer;
 `;
 
 const Copyright = styled.div`
   font-size: 12px;
-  color: #888;
+  color: ${({ theme }) => theme.colors.text.black};
 `;
