9c7611a9200f6975e7e8540cb894cbafa7bceaa2
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-01-20 20:47:03 +0900

    💄 UI: SearchPage Tip 불러오기 수정 #24

--- umc-master/src/pages/search/SearchPage.tsx
+++ umc-master/src/pages/search/SearchPage.tsx
@@ -7,19 +7,62 @@ import RecommendedTipsSection from './components/RecommendTipsSection';
 import dummyImage from '@assets/dummyImage/dummy.jpeg';
 
 const dummyData = [
-  { image: dummyImage, text: '오늘의 추천입니다.' },
-  { image: dummyImage, text: '여행을 떠나요.' },
-  { image: dummyImage, text: '오늘의 추천입니다.' },
-  { image: dummyImage, text: '오늘의 추천입니다.' },
-  { image: dummyImage, text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
-  { image: '#', text: '오늘의추천입니다.' },
-  { image: '#', text: '오늘의 추천입니다.' },
+  {
+    image: dummyImage,
+    text: '따뜻한 감성이 담긴 방꾸미기',
+    likes: 1200,
+    bookmarks: 800,
+    date: '2025.01.20',
+  },
+  {
+    image: dummyImage,
+    text: '여행을 떠나는 방법',
+    likes: 450,
+    bookmarks: 300,
+    date: '2025.01.15',
+  },
+  {
+    image: dummyImage,
+    text: '화사한 인테리어의 비밀',
+    likes: 980,
+    bookmarks: 670,
+    date: '2025.01.18',
+  },
+  {
+    image: dummyImage,
+    text: '북유럽 감성으로 꾸민 침실',
+    likes: 1300,
+    bookmarks: 950,
+    date: '2025.01.12',
+  },
+  {
+    image: dummyImage,
+    text: '오늘의 추천 인테리어 팁',
+    likes: 890,
+    bookmarks: 430,
+    date: '2025.01.10',
+  },
+  {
+    image: dummyImage,
+    text: '화사한 인테리어의 비밀',
+    likes: 980,
+    bookmarks: 670,
+    date: '2025.01.18',
+  },
+  {
+    image: dummyImage,
+    text: '북유럽 감성으로 꾸민 침실',
+    likes: 1300,
+    bookmarks: 950,
+    date: '2025.01.12',
+  },
+  {
+    image: dummyImage,
+    text: '오늘의 추천 인테리어 팁',
+    likes: 890,
+    bookmarks: 430,
+    date: '2025.01.10',
+  },
 ];
 
 const SearchPage = () => {
@@ -38,7 +81,7 @@ const SearchPage = () => {
         onSearch={handleSearch}
         marginTop="80px"
       />
-      <TipsSection items={dummyData.slice(0, 5)} showArrows showLikes={false} />
+      <TipsSection items={dummyData.slice(0, 5)} showLikes={false} />
       <RecommendTitle title={searchValue} />
       <RecommendedTipsSection items={dummyData.slice(0, 8)} />
     </>
