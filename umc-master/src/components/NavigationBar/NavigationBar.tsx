e660f55a3c115602da430ebc0a1d236676754cb2
Minji Kim <minjikim2000@hotmail.com>
2025-01-20 16:12:22 +0900

    💄 UI: update Navigation Bar #25

--- umc-master/src/components/NavigationBar/NavigationBar.tsx
+++ umc-master/src/components/NavigationBar/NavigationBar.tsx
@@ -1,115 +1,98 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { FaRegBell } from 'react-icons/fa';
 import LogoImage from '@assets/logo.png';
 import RoutePaths from '@router/routePaths';
+import Typography from '@components/common/typography';
+import AlarmIcon from '@assets/icons/alarm.svg?react';
 
 interface NavigationBarProps {
   login: boolean;
 }
 
-const NavigationBar: React.FC<NavigationBarProps> = ({ login }) => (
-  <Nav>
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '57px' }}>
-      <Link to={RoutePaths.MAIN}>
-        <Logo src={LogoImage} alt="Logo" />
-      </Link>
-      <MenuItems>
-        <Link to={RoutePaths.MAGAZINE}>매거진</Link>
-        <Link to={RoutePaths.COMMUNITY}>커뮤니티</Link>
-        <Link to={RoutePaths.SAVE_TIP}>저장한 글들</Link>
-      </MenuItems>
-    </div>
-    {login ? (
-      <UserSection>
-        <IconButton>
-          <FaRegBell size={40} />
-        </IconButton>
-        <Profile>
-          <Avatar />
-          <Username>애니님</Username>
-        </Profile>
-      </UserSection>
-    ) : (
-      <LoginLink to={RoutePaths.LOGIN}>로그인</LoginLink>
-    )}
-  </Nav>
-);
-
+const NavigationBar: React.FC<NavigationBarProps> = ({ login }) => {
+  return (
+    <Nav>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: '48px',
+        }}
+      >
+        <Link to={RoutePaths.MAIN}>
+          <Logo src={LogoImage} alt="Logo" />
+        </Link>
+        <Typography variant="titleXxSmall">
+          <MenuItems>
+            <Link to={RoutePaths.MAGAZINE}>매거진</Link>
+            <Link to={RoutePaths.COMMUNITY}>꿀팁나눔</Link>
+            <Link to={RoutePaths.SAVE_TIP}>저장한 꿀팁</Link>
+          </MenuItems>
+        </Typography>
+      </div>
+      {login ? (
+        <UserSection>
+          <AlarmIcon />
+          <ProfileImg />
+        </UserSection>
+      ) : (
+        <LoginBtn to={RoutePaths.LOGIN}>
+          <Typography variant="bodySmall">로그인</Typography>
+        </LoginBtn>
+      )}
+    </Nav>
+  );
+};
 export default NavigationBar;
 
 const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 36px 200px;
-  background: white;
-  border-bottom: 1px solid #eaeaea;
+  width: 100%;
+  height: 80px;
+  margin: 0 auto;
+  padding: 0 clamp(20px, 10vw, 320px);
+  border-bottom: 1px solid ${({ theme }) => theme.colors.text.gray};
 `;
 
 const Logo = styled.img`
-  height: 100px;
+  height: 52px;
   cursor: pointer;
 `;
 
 const MenuItems = styled.div`
   display: flex;
-  gap: 80px;
+  gap: clamp(20px, 10vw, 40px);
 
   a {
-    color: #121212;
-    font-size: 28px;
-    font-family: Pretendard;
-    font-style: medium;
+    color: ${({ theme }) => theme.colors.text.black};
   }
 `;
 
-const LoginLink = styled(Link)`
-  background: #1b8c78;
-  color: white;
-  padding: 12px 64px;
-  border-radius: 12px;
-  font-size: 24px;
-  font-family: Pretendard;
-  font-style: 'semi-bold';
+const LoginBtn = styled(Link)`
+  background: ${({ theme }) => theme.colors.primary[500]};
+  color: ${({ theme }) => theme.colors.text.white};
+  padding: 7px 27px;
+  border-radius: 10px;
   cursor: pointer;
 
   &:hover {
-    background: #177564;
+    background: ${({ theme }) => theme.colors.primary[600]};
   }
-`;
-
-const IconButton = styled.button`
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  height: 70px;
-  color: #084951;
-  cursor: pointer;
 `;
 
 const UserSection = styled.div`
   display: flex;
   align-items: center;
-  gap: 16px;
+  gap: clamp(10px, 10vw, 20px);
+  cursor: pointer;
 `;
 
-const Profile = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
-`;
-
-const Avatar = styled.div`
-  width: 50px;
-  height: 50px;
-  background: #e0e0e0;
+const ProfileImg = styled.div`
+  width: 40px;
+  height: 40px;
+  background: #e0e0e0; /**TODO: 프로필 이미지 추가*/
   border-radius: 50%;
-`;
-
-const Username = styled.span`
-  color: #121212;
-  font-size: 28px;
-  font-family: Pretendard;
-  font-style: medium;
 `;
