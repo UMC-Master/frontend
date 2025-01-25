ca54bfa3d988eaa16dd26840909db7282165cda9
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-01-25 23:33:10 +0900

    💄 UI: SearchSection 재수정 #33

--- umc-master/src/components/SearchBar/SearchSection.tsx
+++ umc-master/src/components/SearchBar/SearchSection.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import SearchTitle from './components/SearchTitle';
 import SearchBar from './components/SearchBar';
 import { useNavigate } from 'react-router-dom';
+import SearchModal from '@components/Modal/search';
+import { useState } from 'react';
 
 interface SearchSectionProps {
   highlight?: string;
@@ -12,6 +14,18 @@ interface SearchSectionProps {
   marginTop?: string;
 }
 
+const dummyCategories = [
+  { section: '계절', tags: ['봄', '여름', '가을', '겨울'] },
+  { section: '패션', tags: ['패션', '맨투맨', '니트', '바지', '치마', '블라우스', '자켓'] },
+  { section: '청소', tags: ['청소', '방', '정리', '인테리어', '가구', '청소도구'] },
+  {
+    section: '요리 / 식재료',
+    tags: ['요리', '음식', '보관', '냉장', '냉동', '면', '밥', '술', '반찬', '레시피', '냉장고'],
+  },
+  { section: '재활용 / 분리수거', tags: ['재활용', '분리수거', '리폼', '플라스틱', '스티로폼', '종이', '유리'] },
+  { section: '주거', tags: ['주택', '원룸', '빌라', '아파트', '기숙사'] },
+];
+
 const SearchSection: React.FC<SearchSectionProps> = ({
   frontText,
   backText,
@@ -19,19 +33,28 @@ const SearchSection: React.FC<SearchSectionProps> = ({
   onSearch,
   marginTop = '0px',
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
   const handleSearch = (value: string) => {
     if (onSearch) {
       onSearch(value);
     }
-    navigate(`/search?query=${value}`);
+    navigate(`/search?${value}`);
   };
 
   return (
-    <Container $marginTop={marginTop}>
-      <SearchTitle frontText={frontText} highlight={highlight} backText={backText} />
-      <SearchBar onSearch={handleSearch} />
-    </Container>
+    <>
+      <Container $marginTop={marginTop} onClick={() => setIsModalOpen(true)}>
+        <SearchTitle frontText={frontText} highlight={highlight} backText={backText} />
+        <SearchBar onSearch={handleSearch} />
+      </Container>
+      <SearchModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSearch={handleSearch}
+        categories={dummyCategories}
+      />
+    </>
   );
 };
 
