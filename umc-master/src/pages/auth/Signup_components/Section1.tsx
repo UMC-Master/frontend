56dad72d9128f481a5f5f7c276a95d0948ff2ac4
rael <ekosemily@gmail.com>
2025-01-31 02:43:06 +0900

    ✨ feat: + 눌렀을 때 약관 상세 내용 띄우기 추가 #21

--- umc-master/src/pages/auth/Signup_components/Section1.tsx
+++ umc-master/src/pages/auth/Signup_components/Section1.tsx
@@ -2,6 +2,7 @@
 import Typography from "@components/common/typography";
 import styled, { useTheme } from "styled-components";
 import ImgAdd from "@assets/add.svg";
+import ImgRemove from "@assets/remove.svg";
 import { useEffect, useState } from "react";
 
 interface AgreementItemProps {
@@ -10,10 +11,13 @@ interface AgreementItemProps {
   isOptional: boolean;  // 선택 사항 여부
   checked: boolean;     // 체크 여부
   onChange: (checked: boolean) => void; // 체크 상태 변경 함수
+  detail: string;       // 이용 약관 상세
 }
 
-const AgreementItem: React.FC<AgreementItemProps> = ({ isRequired, label, isOptional, checked, onChange }) => {
+const AgreementItem: React.FC<AgreementItemProps& { isAllAgreement?: boolean }> = ({ isRequired, label, isOptional, checked, onChange, detail, isAllAgreement = false }) => {
   const theme = useTheme();
+  const [showDetails, setShowDetails] = useState(false);
+
   const labelColor = isRequired
     ? theme.colors.text.gray
     : isOptional
@@ -21,19 +25,33 @@ const AgreementItem: React.FC<AgreementItemProps> = ({ isRequired, label, isOpti
     : theme.colors.text.black;
 
   return (
-    <Agreement>
-      <Terms>
-        <Checkbox type="checkbox" id="agreement" checked={checked} onChange={(e) => onChange(e.target.checked)}/>
-        {isRequired && <Typography variant="bodySmall" style={{ color: theme.colors.red[500] }}>(필수)</Typography>}
-        {!isRequired && isOptional && <Typography variant="bodySmall" style={{ color: theme.colors.blue[400] }}>(선택)</Typography>}
-        <Typography variant="bodySmall" style={{ color: labelColor }}>
-          {label}
-        </Typography>
-      </Terms>
-      <Plus>
-        <img src={ImgAdd} alt="addDetail" />
-      </Plus>
-    </Agreement>
+    <>
+      <Agreement>
+        <Terms>
+          <Checkbox type="checkbox" id="agreement" checked={checked} onChange={(e) => onChange(e.target.checked)}/>
+          {isRequired && <Typography variant="bodySmall" style={{ color: theme.colors.red[500] }}>(필수)</Typography>}
+          {!isRequired && isOptional && <Typography variant="bodySmall" style={{ color: theme.colors.blue[400] }}>(선택)</Typography>}
+          <Typography variant="bodySmall" style={{ color: labelColor }}>
+            {label}
+          </Typography>
+        </Terms>
+        {!isAllAgreement && (
+          <Plus onClick={() => setShowDetails(!showDetails)}>
+            <img src={showDetails ? ImgRemove : ImgAdd} alt="toggleDetails" />
+          </Plus>
+        )}
+      </Agreement>
+      {showDetails && !isAllAgreement && (
+        <Details>
+          <Typography 
+            variant="bodyXSmall"
+            style={{color: theme.colors.text.gray}}
+          >
+            {detail}
+          </Typography>
+        </Details>
+      )}
+    </>
   );
 };
 
@@ -85,6 +103,8 @@ const Section1: React.FC<Section1Props> = ({ onCheckRequired }) => {
         isOptional={false}
         checked={isAllAgreed}
         onChange={handleAllAgreeChange}
+        detail=""
+        isAllAgreement={true}
       />
       <AgreementItem 
         isRequired={true} 
@@ -92,6 +112,7 @@ const Section1: React.FC<Section1Props> = ({ onCheckRequired }) => {
         isOptional={false} 
         checked={checkedItems.terms}
         onChange={(checked) => handleCheckboxChange('terms', checked)}
+        detail="홈마스터 홈페이지 회원에 가입하시면 더욱 더 다양한 서비스를 이용하실 수 있습니다. 우리 사이트는 정보통신망 이용촉진에 관한 법률 등 관련 법령에 따라 아래와 같이 이용자의 동의를 받은 후 회원가입을 받고 있습니다."
       />
       <AgreementItem 
         isRequired={true} 
@@ -99,6 +120,7 @@ const Section1: React.FC<Section1Props> = ({ onCheckRequired }) => {
         isOptional={false} 
         checked={checkedItems.privacy}
         onChange={(checked => handleCheckboxChange('privacy', checked))}
+        detail="홈마스터는 홈페이지 회원 가입 시 회원 서비스 제공에 필요한 최소한의 정보를 수집하고 있으며 개인정보파일에 수집되는 항목은 다음과 같습니다."
       />
       <AgreementItem 
         isRequired={false} 
@@ -106,6 +128,7 @@ const Section1: React.FC<Section1Props> = ({ onCheckRequired }) => {
         isOptional={true} 
         checked={checkedItems.thirdinfo}
         onChange={(checked) => handleCheckboxChange('thirdinfo', checked)}
+        detail="이벤트를 위한 개인 정보를 수집합니다."
       />
       <AgreementItem 
         isRequired={false} 
@@ -113,6 +136,7 @@ const Section1: React.FC<Section1Props> = ({ onCheckRequired }) => {
         isOptional={true} 
         checked={checkedItems.marketing}
         onChange={(checked) => handleCheckboxChange('marketing', checked)}
+        detail="E-mail, SMS 수신에 동의합니다."
       />
     </Container>
   );
@@ -154,4 +178,14 @@ const Plus = styled.button`
   width: 40px;
   height: 40px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Details = styled.div`
+  margin-left: 30px;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.colors.text.white};
+  width: 92%;
 `
