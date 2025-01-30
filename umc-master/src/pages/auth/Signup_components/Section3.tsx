60b7ba9f43d379ebfcab680b2b63d288e544f609
rael <ekosemily@gmail.com>
2025-01-31 04:09:56 +0900

    ✨ feat: Section3 비밀번호 입력 컴포넌트 #21

---
+++ umc-master/src/pages/auth/Signup_components/Section3.tsx
@@ -0,0 +1,29 @@
+import Typography from "@components/common/typography";
+import Input from "@components/Input/Input";
+import { styled, useTheme } from "styled-components";
+
+
+const Section3: React.FC = () => {
+
+  const theme = useTheme();
+  return (
+    <Container>
+      <Typography 
+        variant="headingXxxSmall"
+        style={{color: theme.colors.primary[800]}}
+      >비밀번호 입력 (필수) *</Typography>
+      <Input errorMessage="" type={'password'} placeholder={'비밀번호 입력 (숫자, 영문자, 문자 포함 8~15자 이내)'} />
+      <Input errorMessage="" type={'password'} placeholder={'비밀번호 확인'} />
+    </Container>
+  );
+};
+
+export default Section3;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 30px;
+  align-self: stretch;
+`
