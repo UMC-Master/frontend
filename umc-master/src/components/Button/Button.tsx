418610a5067a1e2bf42cd264fad8ade7e3ef6710
rael <ekosemily@gmail.com>
2025-01-31 00:15:45 +0900

    ✨ feat: button에 pageUp, pageDown, signUp 변수 생성 #21

--- umc-master/src/components/Button/Button.tsx
+++ umc-master/src/components/Button/Button.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
 import styled, { css } from "styled-components";
-
 interface ButtonProps {
-  variant?: "primary" | "kakao";
+  variant?: "primary" | "kakao" | "profileEdit" | "interestEdit" | "pageUp" | "pageDown" | "signUp";
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
-
 const Button: React.FC<ButtonProps> = ({ variant = "primary", children, ...props }) => {
   return (
     <StyledButton variant={variant} {...props}>
@@ -13,10 +12,8 @@ const Button: React.FC<ButtonProps> = ({ variant = "primary", children, ...props
     </StyledButton>
   );
 };
-
 export default Button;
-
-const StyledButton = styled.button<{ variant: "primary" | "kakao" }>`
+const StyledButton = styled.button<{ variant: "primary" | "kakao" | "profileEdit" | "interestEdit" | "pageUp" | "pageDown" | "signUp" }>`
   display: flex;
   width: 616px;
   height: 72px;
@@ -26,13 +23,11 @@ const StyledButton = styled.button<{ variant: "primary" | "kakao" }>`
   border-radius: 20px;
   background: ${({ theme }) => theme.colors.primary[500]};
   cursor: pointer;
-
-  font-family: ${({ theme }) => theme.typography.title.xsmall};
+  font-family: ${({ theme }) => theme.fontFamily.regular};
   font-size: ${({ theme }) => theme.typography.title.xsmall.size};
   font-weight: ${({ theme }) => theme.typography.title.xsmall.weight};
   line-height: ${({ theme }) => theme.typography.title.xsmall.lineHeight};
   letter-spacing: 0.3px;
-
   ${(props) =>
     props.variant === "primary" &&
     css`
@@ -40,10 +35,71 @@ const StyledButton = styled.button<{ variant: "primary" | "kakao" }>`
       color: #fff;
       padding: 29px 204px;
     `}
-
   ${(props) =>
     props.variant === "kakao" &&
     css`
       background: #fee500;
+    `}
+  ${(props) =>
+    props.variant === "profileEdit" &&
+    css`
+      width: 200px;
+      height: 60px;
+      background: ${({ theme }) => theme.colors.primary[500]};
+      color: #fff;
+      padding: 29px;
+      font-size: ${({ theme }) => theme.typography.title.xxxsmall.size};
+      font-weight: ${({ theme }) => theme.typography.title.xxxsmall.weight};
+      line-height: ${({ theme }) => theme.typography.title.xxxsmall.lineHeight};
+    `}
+    
+  ${(props) =>
+    props.variant === "interestEdit" &&
+    css`
+      width: 212px;
+      height: 60px;
+      background: ${({ theme }) => theme.colors.primary[500]};
+      color: #fff;
+      font-size: ${({ theme }) => theme.typography.title.xxxsmall.size};
+      font-weight: ${({ theme }) => theme.typography.title.xxxsmall.weight};
+      line-height: ${({ theme }) => theme.typography.title.xxxsmall.lineHeight};
+    `}
+
+  ${(props) =>
+    props.variant === "pageUp" &&
+    css`
+      width: 160px;
+      height: 72px;
+      background: ${({ theme }) => theme.colors.primary[500]};
+      color: #fff;
+      font-size: ${({ theme }) => theme.typography.title.xxsmall.size};
+      font-weight: ${({ theme }) => theme.typography.title.xxsmall.weight};
+      line-height: ${({ theme }) => theme.typography.title.xxsmall.lineHeight};
+    `}
+
+  ${(props) =>
+    props.variant === "pageDown" &&
+    css`
+      width: 160px;
+      height: 72px;
+      background: ${({ theme }) => theme.colors.text.white};
+      border: 1px solid ${({ theme }) => theme.colors.primary[500]};
+      color: ${({ theme }) => theme.colors.primary[500]};
+      font-size: ${({ theme }) => theme.typography.title.xxsmall.size};
+      font-weight: ${({ theme }) => theme.typography.title.xxsmall.weight};
+      line-height: ${({ theme }) => theme.typography.title.xxsmall.lineHeight};
+    `}
+
+  ${(props) =>
+    props.variant === "signUp" &&
+    css`
+      width: 320px;
+      height: 72px;
+      background: ${({ theme }) => theme.colors.primary[500]};
+      border: 1px solid ${({ theme }) => theme.colors.primary[500]};
+      color: ${({ theme }) => theme.colors.text.white};
+      font-size: ${({ theme }) => theme.typography.title.xxsmall.size};
+      font-weight: ${({ theme }) => theme.typography.title.xxsmall.weight};
+      line-height: ${({ theme }) => theme.typography.title.xxsmall.lineHeight};
     `}
 `;
