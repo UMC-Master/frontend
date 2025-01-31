6f53e232afd04f9f3938b4156c9da150abab9b6c
rael <ekosemily@gmail.com>
2025-02-01 06:06:34 +0900

    ✨ feat: SaveTipPage 구현 #43

--- umc-master/src/pages/saveTip/SaveTipPage.tsx
+++ umc-master/src/pages/saveTip/SaveTipPage.tsx
@@ -1,9 +1,65 @@
+import Card from "@components/Card/Card";
+import Typography from "@components/common/typography";
+import styled, { useTheme } from "styled-components";
+import { dummyData } from "./dummydata/dummydata";
+
+
 const SaveTipPage: React.FC = () => {
+  
+  const theme = useTheme();
   return (
-    <>
-      <h1>SaveTip Page</h1>
-    </>
+    <Container>
+      <SavedTips>
+        <Typography 
+          variant="headingXxSmall"
+          style={{color: theme.colors.primary[900]}}
+        >저장한 꿀팁</Typography>
+        {dummyData.length === 0 ? (
+          <Typography variant="bodySmall">최근 본 꿀팁이 없습니다.</Typography>
+        ) : (
+          <TipCardList>
+            {dummyData.map((item) => (
+              <Card 
+                key={item.id} 
+                image={item.image} 
+                text={item.text} 
+                likes={item.likes || 0} 
+                bookmarks={item.bookmarks || 0} 
+                date={item.date || ''}
+              />
+            ))}
+          </TipCardList>
+        )}
+      </SavedTips>
+    </Container>
   );
 };
 
 export default SaveTipPage;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 80px;
+  padding-bottom: 100px;
+  background: #FFF;
+`
+
+const SavedTips = styled.div`
+  display: flex;
+  width: 1280px;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`
+
+const TipCardList = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  align-self: stretch;
+  flex-wrap: wrap;
+`
---
+++ umc-master/src/pages/saveTip/dummydata/dummydata.ts
@@ -0,0 +1,19 @@
+import Image from './image.png'
+
+export const dummyData = [
+  { id: "1", image: Image, text: "자취생을 위한 꿀팁 1", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "2", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "3", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "4", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "5", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "6", image: Image, text: "자취생을 위한 꿀팁 1", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "7", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "8", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "9", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "10", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "11", image: Image, text: "자취생을 위한 꿀팁 1", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "12", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "13", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "14", image: Image, text: "청소 쉽게 하는 방법", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+  { id: "15", image: Image, text: "절약 꿀팁 3가지", likes: 1500, bookmarks: 1500, date: "2025.01.01" },
+];
