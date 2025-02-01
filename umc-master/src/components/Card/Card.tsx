32e566056ed9f1f28caee9dd517444b65db36cf5
Minji Kim <68258295+minzee09@users.noreply.github.com>
2025-02-02 03:23:00 +0900

    Feat/#36 UI magazine (#51)
    * 🔧 config: framer-motion 버전 수정 #36
    
    * ✨ feat: add mind-map component #36
    
    * ✨ feat: add card-grid component #36
    
    * ♻️ refactor: card에서 cardInfo 분리 #36
    
    * 💄 UI: add MagazinePage #36
    
    * 💄 UI: add MagazineDetailPage #36

--- umc-master/src/components/Card/Card.tsx
+++ umc-master/src/components/Card/Card.tsx
@@ -1,6 +1,6 @@
-import Typography from '@components/common/typography';
 import React from 'react';
 import styled from 'styled-components';
+import CardInfo from './CardInfo';
 
 interface CardProps {
   image: string;
@@ -17,28 +17,7 @@ const Card: React.FC<CardProps> = ({ image, text, likes, bookmarks, date, onClic
       <CardImageWrapper>
         <CardImage src={image} alt={text} />
       </CardImageWrapper>
-      <CardText>
-        <Typography variant="titleXxxSmall">{text}</Typography>
-      </CardText>
-      <CardDetails>
-        <IconWrapper>
-          <HeartIcon className="fas fa-heart" />
-          <Typography variant="captionDefault">
-            <Count>{likes.toLocaleString()}</Count>
-          </Typography>
-        </IconWrapper>
-        <IconWrapper>
-          <BookmarkIcon className="fas fa-bookmark" />
-          <Typography variant="captionDefault">
-            <Count>{bookmarks.toLocaleString()}</Count>
-          </Typography>
-        </IconWrapper>
-      </CardDetails>
-      <DateWrapper>
-        <Typography variant="captionDefault">
-          <CardDate>{date}</CardDate>
-        </Typography>
-      </DateWrapper>
+      <CardInfo text={text} likes={likes} bookmarks={bookmarks} date={date} />
     </CardContainer>
   );
 };
@@ -65,42 +44,4 @@ const CardImage = styled.img`
   width: 100%;
   height: 200px;
   object-fit: cover;
-`;
-const CardText = styled.div`
-  margin: 3px 0;
-`;
-
-const CardDetails = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-`;
-
-const IconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 6px;
-`;
-
-const HeartIcon = styled.i`
-  font-size: 16px;
-  color: #ff6b6b;
-`;
-
-const BookmarkIcon = styled.i`
-  font-size: 16px;
-  color: #3498db;
-`;
-
-const Count = styled.span`
-  color: #121212;
-`;
-
-const DateWrapper = styled.div`
-  margin-top: 6px;
-`;
-
-const CardDate = styled.span`
-  color: ${({ theme }) => theme.colors.text[500]};
 `;
