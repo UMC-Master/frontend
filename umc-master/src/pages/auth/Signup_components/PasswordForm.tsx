8d0df3282822206b16bfa1878c46c5e590229c76
Rael <emilyekos@gmail.com>
2025-02-08 03:19:26 +0900

    ✨ feat: 비밀번호 입력 상태 검증 및 에러메시지 추가 #66

--- umc-master/src/pages/auth/Signup_components/PasswordForm.tsx
+++ umc-master/src/pages/auth/Signup_components/PasswordForm.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import Typography from "@components/common/typography";
 import Input from "@components/Input/Input";
+import useInput from "@hooks/useInput";
+import { validatePasswordFormat } from "@utils/validation";
 import { useEffect, useState } from "react";
 import { styled, useTheme } from "styled-components";
 
@@ -8,17 +10,45 @@ import { styled, useTheme } from "styled-components";
 const PasswordForm: React.FC<{ onCheckRequired: (isValid: boolean) => void }> = ({ onCheckRequired }) => {
 
   const theme = useTheme();
-
-  const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('');
+  const [confirmPasswordErrorMessage, setConfirmPasswordErrorMessage] = useState<string>('');
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(false);
+
+  // 비밀번호 상태 검증 및 에러메세지
+  const {
+    input: password, 
+    errorMessage: passwordInputErrorMessage,
+    changeHandler: passwordChangeHandler,
+  } = useInput({
+    initialValue: "",
+    validate: async (value) => {
+      const error = validatePasswordFormat(value);
+      setIsPasswordValid(!error); // 형식이 맞으면 true, 아니면 false
+      return error;
+    }
+  });
+
+  // 비밀번호 확인 오류 메시지 실시간 변동
+  useEffect(() => {
+    if (confirmPassword && password !== confirmPassword) {
+      setConfirmPasswordErrorMessage("비밀번호가 일치하지 않습니다.");
+    } else {
+      setConfirmPasswordErrorMessage("");
+    }
+  }, [confirmPassword, password]);
 
   useEffect(() => {
-    if (password && confirmPassword && password === confirmPassword) {
+      setPasswordErrorMessage(passwordInputErrorMessage || "");
+  }, [passwordInputErrorMessage]);
+
+  useEffect(() => {
+    if (isPasswordValid && confirmPassword && password === confirmPassword) {
       onCheckRequired(true);
     } else {
       onCheckRequired(false);
     }
-  }, [password, confirmPassword]);
+  }, [isPasswordValid, password, confirmPassword]);
   
   return (
     <Container>
@@ -27,14 +57,14 @@ const PasswordForm: React.FC<{ onCheckRequired: (isValid: boolean) => void }> = 
         style={{color: theme.colors.primary[700]}}
       >비밀번호 입력 (필수) *</Typography>
       <Input 
-        errorMessage="" 
+        errorMessage={passwordErrorMessage} 
         type={'password'} 
         placeholder={'비밀번호 입력 (숫자, 영문자, 문자 포함 8~15자 이내)'}
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={passwordChangeHandler}
       />
       <Input 
-        errorMessage="" 
+        errorMessage={confirmPasswordErrorMessage} 
         type={'password'} 
         placeholder={'비밀번호 확인'} 
         value={confirmPassword}
@@ -46,7 +76,7 @@ const PasswordForm: React.FC<{ onCheckRequired: (isValid: boolean) => void }> = 
 
 export default PasswordForm;
 
-const Container = styled.div`
+const Container = styled.form`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
