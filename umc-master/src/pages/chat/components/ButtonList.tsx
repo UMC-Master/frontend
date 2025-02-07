5cf35fccbd6da1bcd0e1824a94711380661655d3
Minji Kim <minjikim2000@hotmail.com>
2025-02-08 01:05:04 +0900

    💄 UI: 챗봇 버튼 컴포넌트 추가 #60

---
+++ umc-master/src/pages/chat/components/ButtonList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import Typography from '@components/common/typography';
+
+interface ButtonListProps {
+  buttonTexts: string[];
+}
+
+const ButtonList: React.FC<ButtonListProps> = ({ buttonTexts }) => (
+  <ButtonContainer>
+    {buttonTexts.map((text, index) => (
+      <ActionButton key={index}>
+        <Typography variant="titleXxxSmall">{text}</Typography>
+      </ActionButton>
+    ))}
+  </ButtonContainer>
+);
+
+export default ButtonList;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const ActionButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary[500]};
+  color: ${({ theme }) => theme.colors.text.white};
+  border: none;
+  border-radius: 20px;
+  padding: 16px 40px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary[600]};
+  }
+`;
