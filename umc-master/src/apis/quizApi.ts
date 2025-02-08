65cecf94b166fbca8dd408b27553ecb8306327fe
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-02-09 00:50:33 +0900

    ✨ feat: Quiz Hook 구현 #64

---
+++ umc-master/src/apis/quizApi.ts
@@ -0,0 +1,34 @@
+import axiosInstance from './axios-instance';
+
+export interface QuizData {
+  quiz_id: number;
+  question: string;
+  answer: number;
+  description: string;
+}
+
+export interface QuizResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: {
+    response: {
+      number_of_quiz: number;
+      quiz_list: QuizData[];
+    };
+  };
+}
+
+export const fetchQuizData = async (): Promise<QuizResponse> => {
+  const response = await axiosInstance.get<QuizResponse>('/quizzes', {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+    },
+  });
+
+  if (!response?.data || !response.data.result) {
+    throw new Error('퀴즈 데이터가 없습니다.');
+  }
+
+  return response.data;
+};
