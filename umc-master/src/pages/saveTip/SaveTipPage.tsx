28835fc169c21ccd79b7bd17ae97a397fb62ada7
rael <emilyekos@gmail.com>
2025-02-17 04:23:41 +0900

    ✨ feat: api 연결 중 #76

--- umc-master/src/pages/saveTip/SaveTipPage.tsx
+++ umc-master/src/pages/saveTip/SaveTipPage.tsx
@@ -1,58 +1,54 @@
 import Card from "@components/Card/Card";
 import Typography from "@components/common/typography";
 import styled, { useTheme } from "styled-components";
-import { dummyData as initialData } from "./dummydata/dummydata";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import SkeletonCard from "@components/Skeleton/SkeletonCard";
 import { useNavigate } from "react-router-dom";
 import { recentStore } from "@store/recentStore";
+import { useSaveTipList } from "@apis/queries/useSaveTipQueries";
 
 const PAGE_SIZE = 5;
 
 const SaveTipPage: React.FC = () => {
   
   const theme = useTheme();
-
   const { addRecentTip } = recentStore();
-  const [data, setData] = useState(initialData.slice(0, PAGE_SIZE * 6));
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(initialData.length > PAGE_SIZE);
+  const navigate = useNavigate();
   const observerRef = useRef<IntersectionObserver | null>(null);
   const lastElementRef = useRef<HTMLDivElement | null>(null);
 
-  const loadMoreData = useCallback(() => {
-    if (isLoading || !hasMore) return;
+  const {
+    data, 
+    fetchNextPage, 
+    hasNextPage, 
+    isFetchingNextPage, 
+  } = useSaveTipList();
 
-    setIsLoading(true);
-    setTimeout(() => {
-      const nextData = initialData.slice(data.length, data.length + PAGE_SIZE);
-      setData((prevData) => [...prevData, ...nextData]);
-      setHasMore(data.length + PAGE_SIZE < initialData.length);
-      setIsLoading(false);
-    }, 1000);
-  }, [isLoading, hasMore, data.length]);
+  const loadMoredata = useCallback(() => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   useEffect(() => {
-    if (isLoading || !hasMore) return;
+    if (!hasNextPage) return;
 
-    if (observerRef.current) observerRef.current.disconnect();
+    if(observerRef.current) observerRef.current.disconnect();
 
     observerRef.current = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        loadMoreData();
+        loadMoredata();
       }
     });
 
     if (lastElementRef.current) observerRef.current.observe(lastElementRef.current);
 
     return () => observerRef.current?.disconnect();
-  }, [isLoading, hasMore, loadMoreData]);
-
-  const navigate = useNavigate(); // 추가
+  }, [hasNextPage, loadMoredata]);
 
   const handleCardClick = (id: string) => {
     // 클릭 시 팁을 최근에 본 리스트에 추가
-    const clickedTip = data.find((item) => item.id === id);
+    const clickedTip = data?.pages.flatMap((page) => page.tips).find((item) => item.id === id);
     if (clickedTip) {
       addRecentTip(clickedTip);
     }
@@ -67,11 +63,11 @@ const SaveTipPage: React.FC = () => {
           variant="headingXxSmall"
           style={{color: theme.colors.primary[900]}}
         >저장한 꿀팁</Typography>
-        {data.length === 0 && !isLoading ? (
+        {data?.pages.length === 0 && !isFetchingNextPage ? (
           <Typography variant="bodySmall">최근 본 꿀팁이 없습니다.</Typography>
         ) : (
           <TipCardList>
-            {data.map((item) => (
+            {data?.pages.flatMap((page) => page.tips).map((item) => (
               <Card 
                 key={item.id} 
                 image={item.image} 
@@ -84,10 +80,10 @@ const SaveTipPage: React.FC = () => {
             ))}
             
             {/* 마지막 요소 감지용 div */}
-            {hasMore && !isLoading && <div ref={lastElementRef} style={{ height: "10px" }} />}
+            {hasNextPage && !isFetchingNextPage && <div ref={lastElementRef} style={{ height: "10px" }} />}
 
             {/* 스켈레톤 UI */}
-            {isLoading && 
+            {isFetchingNextPage && 
               Array.from({ length: PAGE_SIZE }).map((_, index) => (
                 <SkeletonCard key={`skeleton-${index}`} />
               ))
