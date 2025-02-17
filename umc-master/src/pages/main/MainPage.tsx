bc8ddb062ea1c2a72c5c594100d1646dc78ce627
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-02-17 16:17:33 +0900

    💄 UI: ChatBot Icon 추가 #85

--- umc-master/src/pages/main/MainPage.tsx
+++ umc-master/src/pages/main/MainPage.tsx
@@ -4,9 +4,17 @@ import QuizBox from './components/QuizBox';
 import SearchSection from '../../components/SearchBar/SearchSection';
 import TipsSection from './components/TipsSection';
 import { useQuizStore } from '@store/quizStore';
+import ChatBotIcon from '@assets/icons/chatbot.svg?react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const MainPage: React.FC = () => {
   const { isQuizVisible } = useQuizStore();
+  const navigate = useNavigate();
+  const handleClickBot = () => {
+    navigate(`/chat`);
+    window.scroll(0, 0);
+  };
 
   return (
     <>
@@ -15,12 +23,22 @@ const MainPage: React.FC = () => {
         {isQuizVisible && <QuizBox />}
         <SearchSection frontText="궁금한" highlight="키워드" backText="를 검색해보세요!" marginTop="60px" />
         <InterestsAndCategories />
-        <TipsSection title="이 주의 꿀팁 BEST 5" showLikes defaultSort="likes" />
-        <TipsSection title="실시간 꿀팁" showArrows showLikes={false} defaultSort="latest" />
-        <TipsSection title="오늘의 꿀팁" showArrows defaultSort="likes" />
+        <FixedChatbotWrapper>
+          <ChatBotIcon onClick={handleClickBot} />
+        </FixedChatbotWrapper>
+        <TipsSection title="이 주의 꿀팁 BEST 5" showLikes defaultSort="likes" timeFilter="7days" />
+        <TipsSection title="실시간 꿀팁" showArrows showLikes={false} defaultSort="latest" timeFilter="24h" />
+        <TipsSection title="오늘의 꿀팁" showArrows defaultSort="likes" timeFilter="today" />
       </>
     </>
   );
 };
 
 export default MainPage;
+
+const FixedChatbotWrapper = styled.div`
+  position: fixed;
+  right: 80px;
+  bottom: 40px;
+  cursor: pointer;
+`;
