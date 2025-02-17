49a9e4b9b264b8b0828b81138f61ff4ca2cb16a3
Minji Kim <minjikim2000@hotmail.com>
2025-02-17 14:26:32 +0900

    🔨 fix: 라우트 머지 해결 #60

---
+++ umc-master/src/store/tokenStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+
+interface TokenStore {
+  accessToken: string | null;
+  refreshToken: string | null;
+  setTokens: (tokens: { accessToken: string; refreshToken: string }) => void;
+  clearTokens: () => void;
+}
+
+export const useTokenStore = create<TokenStore>((set) => ({
+  accessToken: null,
+  refreshToken: `${import.meta.env.VITE_REFRESH_TOKEN}` /* 로그인 기능 완성 후 수정 예정임 */,
+  setTokens: (tokens) => {
+    // console.log('Setting Tokens:', tokens);
+    set(() => ({
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+    }));
+    // console.log('Tokens after Setting:', useTokenStore.getState());
+  },
+  clearTokens: () => {
+    set(() => ({
+      accessToken: null,
+      refreshToken: null,
+    }));
+    // console.log('Tokens Cleared:', useTokenStore.getState());
+  },
+}));
