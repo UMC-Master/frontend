d231fb23fe7e710ea2d9dd6bc2d7d5b7bbb0d27f
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-02-19 05:28:18 +0900

    ✨ feat: user HashTag Get, Post #85

--- umc-master/src/pages/main/MainPage.tsx
+++ umc-master/src/pages/main/MainPage.tsx
@@ -7,14 +7,27 @@ import { useQuizStore } from '@store/quizStore';
 import ChatBotIcon from '@assets/icons/chatbot.svg?react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '@store/userStore';
+import { useEffect } from 'react';
 
 const MainPage: React.FC = () => {
   const { isQuizVisible } = useQuizStore();
+  const { user, fetchUser } = useUserStore();
   const navigate = useNavigate();
   const handleClickBot = () => {
     navigate(`/chat`);
     window.scroll(0, 0);
   };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  if (!user?.hashtags) {
+    return null; // 또는 로딩 컴포넌트 표시
+  }
+
+  console.log(user?.hashtags);
 
   return (
     <>
@@ -22,13 +35,19 @@ const MainPage: React.FC = () => {
         <Banner />
         {isQuizVisible && <QuizBox />}
         <SearchSection frontText="궁금한" highlight="키워드" backText="를 검색해보세요!" marginTop="60px" />
-        <InterestsAndCategories />
+        <InterestsAndCategories userHashtags={user?.hashtags} />
         <FixedChatbotWrapper>
           <ChatBotIcon onClick={handleClickBot} />
         </FixedChatbotWrapper>
         <TipsSection title="이 주의 꿀팁 BEST 5" showLikes defaultSort="likes" timeFilter="7days" />
         <TipsSection title="실시간 꿀팁" showArrows showLikes={false} defaultSort="latest" timeFilter="24h" />
-        <TipsSection title="오늘의 꿀팁" showArrows defaultSort="likes" timeFilter="today" />
+        <TipsSection
+          tags={user?.hashtags}
+          title={`${user?.nickname} 님의 관심사`}
+          showArrows
+          defaultSort="likes"
+          isSearchSection
+        />
       </>
     </>
   );
