26027e78e178fa7c289b9d0aae7623fc6b262b84
Minji Kim <minjikim2000@hotmail.com>
2025-02-20 21:04:47 +0900

    🐛 bug: 타입 에러 해결 #76

--- umc-master/src/apis/queries/useCommentMutations.ts
+++ umc-master/src/apis/queries/useCommentMutations.ts
@@ -2,12 +2,20 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { getComments, addComment, editComment, deleteComment } from '@apis/commentApi';
 import { useUserStore } from '@store/userStore';
 
+interface Comment {
+  comment_id: number;
+  user: {
+    user_id: number;
+  };
+  comment: string;
+}
+
 export const useAddComment = (tipId: number) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (comment: string) => addComment(tipId.toString(), comment),
     onSuccess: () => {
-      queryClient.invalidateQueries(['comments', tipId]);
+      queryClient.invalidateQueries({ queryKey: ['comments', tipId] });
     },
   });
 };
@@ -17,15 +25,15 @@ export const useDeleteComment = (tipId: number) => {
   const { user } = useUserStore();
   return useMutation({
     mutationFn: async (commentId: number) => {
-      const comments = await getComments(tipId);
-      const comment = comments.find((c: any) => c.comment_id === commentId);
+      const comments: Comment[] = await getComments(tipId);
+      const comment = comments.find((c) => c.comment_id === commentId);
       if (comment?.user.user_id !== user?.user_id) {
         throw new Error('본인의 댓글만 삭제할 수 있습니다.');
       }
       return deleteComment(tipId.toString(), commentId.toString());
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(['comments', tipId]);
+      queryClient.invalidateQueries({ queryKey: ['comments', tipId] });
     },
   });
 };
@@ -35,15 +43,15 @@ export const useUpdateComment = (tipId: number) => {
   const { user } = useUserStore();
   return useMutation({
     mutationFn: async ({ commentId, newComment }: { commentId: number; newComment: string }) => {
-      const comments = await getComments(tipId);
-      const comment = comments.find((c: any) => c.comment_id === commentId);
+      const comments: Comment[] = await getComments(tipId);
+      const comment = comments.find((c) => c.comment_id === commentId);
       if (comment?.user.user_id !== user?.user_id) {
         throw new Error('본인의 댓글만 수정할 수 있습니다.');
       }
       return editComment(tipId.toString(), commentId.toString(), newComment);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(['comments', tipId]);
+      queryClient.invalidateQueries({ queryKey: ['comments', tipId] });
     },
   });
 };
