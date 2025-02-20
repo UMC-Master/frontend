2aee74ed10ce84247e52a228b131c104aebd5691
Minji Kim <minjikim2000@hotmail.com>
2025-02-20 21:01:42 +0900

    ✨ feat: 인터페이스 속성 반영 #76

--- umc-master/src/pages/mypage/components/RecentTips.tsx
+++ umc-master/src/pages/mypage/components/RecentTips.tsx
@@ -6,44 +6,42 @@ import { recentStore } from '@store/recentStore';
 import { useEffect } from 'react';
 
 const RecentTips: React.FC = () => {
-
   const theme = useTheme();
 
   const navigate = useNavigate();
 
   // zustand 상태에서 최근 팁 가져오기
   const { recentTips } = recentStore();
-  
+
   const handleCardClick = (id: string) => {
     navigate(`/save-tip/${id}`); // 상세 페이지로 이동
   };
 
   useEffect(() => {
     // 처음 렌더링 시에 최근 본 팁이 로컬스토리지에 있으면 복원됩니다.
-  }, []); 
+  }, []);
 
   return (
     <RecentGoodTip>
-      <Typography 
-        variant='titleXxSmall' 
-        style={{color: theme.colors.primary[800]}}
-      >최근에 본 꿀팁</Typography>
+      <Typography variant="titleXxSmall" style={{ color: theme.colors.primary[800] }}>
+        최근에 본 꿀팁
+      </Typography>
       {recentTips.length === 0 ? (
         <Typography variant="bodySmall">최근 본 꿀팁이 없습니다.</Typography>
       ) : (
         <TipCardList>
           {recentTips.map((item) => (
-            <Card 
-              key={item.id} 
-              image={item.image} 
-              text={item.text} 
-              likes={item.likes || 0} 
-              bookmarks={item.bookmarks || 0} 
-              date={item.date || ''}
-              onClick={() => handleCardClick(item.id)}
+            <Card
+              key={item.tipId}
+              image={item.imageUrls[0].media_url}
+              text={item.title}
+              likes={item.likesCount || 0}
+              bookmarks={item.savesCount || 0}
+              date={item.createdAt || ''}
+              onClick={() => handleCardClick(String(item.tipId))}
             />
           ))}
-      </TipCardList>
+        </TipCardList>
       )}
     </RecentGoodTip>
   );
@@ -59,11 +57,11 @@ const RecentGoodTip = styled.div`
   align-items: flex-start;
   gap: 20px;
   flex-shrink: 0;
-`
+`;
 
 const TipCardList = styled.div`
   display: flex;
   align-items: center;
   gap: 30px;
   align-self: stretch;
-`
+`;
