9efad60ea8874aa78776ac2ba3d3afffc6b8c75a
rael <emilyekos@gmail.com>
2025-02-20 19:33:13 +0900

    ♻️ refactor: FloatingToggleBtn 컴포넌트 분리 #90

--- umc-master/src/pages/challenge/ChallengeDetailPage.tsx
+++ umc-master/src/pages/challenge/ChallengeDetailPage.tsx
@@ -1,16 +1,9 @@
 import styled from "styled-components";
 import ChallengeComment from "./DetailPage_component/ChallengeComment";
 import ChallengeDetail from "./DetailPage_component/ChallengeDetail";
-import Likes from "@assets/savetipdetail/Likes.svg";
-import Liked from "@assets/savetipdetail/liked.svg";
-import Saves from "@assets/savetipdetail/saves.svg";
-import Saved from "@assets/savetipdetail/saved.svg";
-import Link from "@assets/savetipdetail/link.svg";
-import Typography from "@components/common/typography";
-import { useParams } from "react-router-dom";
-import theme from "@styles/theme";
-import { useState } from "react";
 import { ChallengeDetailPageDataList, dummyData } from "@pages/challenge/dummydata/dummydata";
+import FloatingToggleBtn from "./DetailPage_component/FloatingToggleBtn";
+import { useParams } from "react-router-dom";
 
 const ChallengeDetailPage: React.FC = () => {
 
@@ -22,62 +15,14 @@ const ChallengeDetailPage: React.FC = () => {
     return <div>데이터를 찾을 수 없습니다.</div>;
   }
   
-  const [likes, setLikes] = useState(detail.likes);
-  const [liked, setLiked] = useState(false);
-
-  const handleLikeClick = () => {
-    if (!liked) {
-      setLikes(likes + 1); // 좋아요 수 증가
-      setLiked(true); // 좋아요 상태로 변경
-    } else {
-      setLikes(likes - 1); // 좋아요 취소 시 수 감소
-      setLiked(false); // 좋아요 취소 상태로 변경
-    }
-  };
-
-  const [saves, setSaves] = useState(detail.bookmarks);
-  const [saved, setSaved] = useState(false);
-
-  const handleSaveClick = () => {
-    if (!saved) {
-      setSaves(saves + 1); // 좋아요 수 증가
-      setSaved(true); // 좋아요 상태로 변경
-    } else {
-      setSaves(saves - 1); // 좋아요 취소 시 수 감소
-      setSaved(false); // 좋아요 취소 상태로 변경
-    }
-  };
-  
   return (
     <Container>
-      <ChallengeDatail>
+      <Content>
         <ChallengeDetail detail={ChallengeDetailPageDataList[0]} />
-        <Line/>
-        <ChallengeComment/>
-      </ChallengeDatail>
-      <InteractionButtons>
-        <Interaction onClick={handleLikeClick}>
-          <Img src={liked ? Liked : Likes} alt="좋아요"/>
-          <Typography 
-            variant="bodyXSmall"
-            style={{color: theme.colors.text.lightGray}}
-          >{likes}</Typography>
-        </Interaction>
-        <Interaction onClick={handleSaveClick}>
-          <Img src={saved ? Saved : Saves} alt="저장하기"/>
-          <Typography 
-            variant="bodyXSmall"
-            style={{color: theme.colors.text.lightGray}}
-          >{saves}</Typography>
-        </Interaction>
-        <Interaction>
-          <Img src={Link} alt="공유하기"/>
-          <Typography 
-            variant="bodyXSmall"
-            style={{color: theme.colors.text.lightGray}}
-          >공유하기</Typography>
-        </Interaction>
-      </InteractionButtons>
+        <Line />
+        <ChallengeComment />
+      </Content>
+      <FloatingToggleBtn />
     </Container>
   );
 };
@@ -95,7 +40,7 @@ const Container = styled.div`
   background: #FFF;
 `
 
-const ChallengeDatail = styled.div`
+const Content = styled.div`
   display: flex;
   width: 1280px;
   flex-direction: column;
@@ -108,29 +53,4 @@ const Line = styled.div`
   width: 1280px;
   height: 1px;
   border: 1px solid ${({ theme }) => theme.colors.primary[800]};
-`
-
-const InteractionButtons = styled.div`
-  position: fixed;
-  right: 168px;
-  top: 610px;
-  width: 72px;
-  height: 366px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 36px;
-`
-
-const Interaction = styled.div`
-  display: flex;
-  width: 72px;
-  flex-direction: column;
-  align-items: center;
-  gap: 7px;
-`
-
-const Img = styled.img`
-  object-fit: cover;
-  cursor: pointer;
 `
